a2ab757c579068316a05cf24ed9d474f6b65afcf
4our6ixintel <hytsfx412@gmail.com>
2025-12-03 13:58:59 +0300

    Create firebase.js

---
+++ firebase.js
@@ -0,0 +1,17 @@
+  // Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.6.0/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyAT_8BYUfGbf5TL2gYk7kU2OlkdPJJDsK0",
+    authDomain: "admin-panel-4237d.firebaseapp.com",
+    projectId: "admin-panel-4237d",
+    storageBucket: "admin-panel-4237d.firebasestorage.app",
+    messagingSenderId: "581017219442",
+    appId: "1:581017219442:web:c9743c5ec53b5c55a2edeb"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);

636770b6e8c1b2097f667e9b730384de8e2a89ba
4our6ixintel <hytsfx412@gmail.com>
2026-04-07 23:14:51 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,15 +1,20 @@
-// firebase.js
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyAT_8BYUfGbf5TL2gYk7kU2OlkdPJJDsK0",
   authDomain: "admin-panel-4237d.firebaseapp.com",
   projectId: "admin-panel-4237d",
-  storageBucket: "admin-panel-4237d.appspot.com",
+  storageBucket: "admin-panel-4237d.firebasestorage.app",
   messagingSenderId: "581017219442",
   appId: "1:581017219442:web:c9743c5ec53b5c55a2edeb"
 };
 
 // Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+const app = initializeApp(firebaseConfig);
 
 // Firestore & Auth
 const db = firebase.firestore();
